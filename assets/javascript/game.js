f53edb8a735a35357a4eb718a3c7cd81d134971d
TracyTN <tracytn0411@gmail.com>
2019-07-10 15:24:49 -0700

    Update onekeyup event.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,4 @@
-//? var vs. let vs. const
-    //* var is function scoped, let & const are blocked scoped (inside curly bracket)
-    //* Redeclare var inside block will also redeclare var outside block (hoisting) -> problem!
-        //~ should use let to declare variable inside block
-    //* var & let can be re-assigned, const cannot
-        //~ should use const unless you know variable gonna change
-        //~ const let whoever read your code know that this variable shouldn't change (stay constant)
+
 
 var selectableSongs = ["california love",
             "i swear",
@@ -30,8 +24,8 @@ var winCount = 0;
 var guessLeft = 9;
 var currentSongIndex; //Index of current song in the array
 var guessedLetters = [];
-var guessingSong = []; //Song user is trying to guess (to match current song index)
-var remainingLetters; //Keep track of how many letters left to be guessed --> //|| Check for win
+var guessingSong = ""; //Song user is trying to guess (to match current song index)
+var remainingLetters; //Keep track of how many letters left to be guess --> //|| Check for win
 
 var gameStarted = false; //flag to tell game has started
 var gameFinished = false; //flag to 'press any key to start again!'
@@ -43,17 +37,14 @@ reset();
 function reset() {
     guessLeft = 9;
     guessedLetters = [];
-    guessingSong = [];
+    guessingSong = "";
     gameStarted = false;
 
-    //Computer picks random song index and log in console
+    //Computer picks random song index for user to guess
     currentSongIndex = selectableSongs[Math.floor(Math.random()*selectableSongs.length)];
     console.log("Current song to guess: " + currentSongIndex);
 
-    for (var i=0; i<currentSongIndex.length; i++) {
-        guessingSong.push("_");
-    }
-    //Keep track of how many letters left to guess
+    //Keep track of how many letters left to guess --> //|| Check for win
     remainingLetters = currentSongIndex.length; 
     
     //Hide gameover and win image
@@ -62,60 +53,74 @@ function reset() {
 
     //update HTML display
     updateDisplay();
-
 }
 
-//? innerText vs. innerHTML
-    //*innerText sets content of tag as plain text, ignore HTML format
-    //*innerHTML sets content in HTML format --> enter HTML code directly
 
 //function to update display on HTML page
 function updateDisplay(){
     document.querySelector(".totalWins").innerText = winCount;
-
-    document.querySelector(".currentSong").innerHTML = "";
-    for (var i=0; i<guessingSong.length; i++){
-        document.querySelector(".currentSong").innerHTML += guessingSong[i];
-    }
-
+    document.querySelector(".currentSong").innerText = guessingSong;
     document.querySelector(".remainingGuesses").innerHTML = guessLeft;
     document.querySelector(".guessedLetters").innerHTML = guessedLetters;
-    
-    //check for lose, if true then ask user to press any key
-    if (guessLeft == 0){
-        gameFinished = true;
-        document.querySelector("#gameover-image").style.display = "block";
-        document.querySelector("#pressKeyTryAgain").style.display = "block";
-    }
+}
 
 
-//function to capture user's keyboard input
-document.onkeyup = function (even) {
+//Capture user's pressed key and store it into userInput
+document.onkeyup = function(event) {
     var userInput = event.key;
     console.log (userInput);
 
-
-    //check if the game has finished
     if (gameFinished) {
-         reset();
-         gameFinished = false;
-    } else {
-        //Check to make sure a-z was pressed
-        if(event.keyCode >= 65 && event.keyCode <= 90){
-            event.key.toLowerCase();
+        //If game has finised, reset the game
+        reset();
+        gameFinished = false;
+    }else{
+        if (currentSongIndex.indexOf(userInput) > -1) {
+            //If user's pressed key is in the song, run correctGuess function
+            correctGuess(userInput);
+        }else{
+            //If user's pressed key is NOT in the song, run incorrectGuess function
+            incorrectGuess(userInput);
         }
     }
+}
 
-    for (var i=0; i<currentSongIndex.length; i++) {
-        if (guessedLetters.includes(currentSongIndex[i])) {
-            guessingSong[i] += guessedLetters;
-        } else {
-            guessingSong[i] += "_";
+//Evaluate the incorrectGuess
+function incorrectGuess(userInput){
+    if (guessedLetters.indexOf(userInput) < 0){
+        //If the incorrect pressed key has NOT beed added to the guessLetters list yet, add it.
+        //This is to prevent choosing the same incorrect key twice
+        guessedLetters.push(userInput);
+        guessLeft--;
+        updateDisplay();
+        if (guessLeft == 0){
+            //If guessLeft is 0, user loses
+            document.querySelector("#gameover-image").style.display = "block";
+            document.querySelector("#pressKeyTryAgain").style.display = "block";
+            gameFinished = true;
         }
     }
-    guessedLetters.push(userInput);
-    guessLeft--;
-    updateDisplay();
 }
 
+
+//evaluate correct guess
+function correctGuess(userInput){
+    if (guessingSong.indexOf(userInput) < 0){
+    for (var i=0; i<currentSongIndex.length; i++){
+        if (userInput.includes(currentSongIndex[i])) {
+            guessingSong += currentSongIndex[i];
+            updateDisplay();
+            remainingLetters--;
+            if (remainingLetters == 0){
+                document.querySelector("#youwin-image").style.display = "block";
+                document.querySelector("#pressKeyTryAgain").style.display = "block";
+                gameFinished = true;
+            }
+        }
+    }
+}   
 }
+
+
+
+
